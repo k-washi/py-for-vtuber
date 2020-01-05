8de7aa9f974de2521db1636338c39469ab3da393
k-washi <kai.washizaki@gmail.com>
2020-01-06 08:34:45 +0900

    Add hand detector by SSD

--- detector/handDetector.py
+++ detector/handDetector.py
@@ -3,4 +3,86 @@ Victor Dibia, HandTrack: A Library For Prototyping Real-time Hand TrackingInterf
 using Convolutional Neural Networks, https://github.com/victordibia/handtracking
 
 """
-#
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+#-----
+
+import tensorflow as tf
+import numpy as np
+
+#-----
+
+detection_graph = tf.Graph()
+_score_thresh = 0.3
+
+MODEL_ROOT = './model'
+PATH_TO_CKPT = os.path.join(MODEL_ROOT,  'frozen_inference_graph.pb')
+PATH_TO_LABEL = os.path.join(MODEL_ROOT, 'hand_label_map.pbtxt')
+
+NUM_CLASS = 1
+
+def load_inference_graph():
+  # load frozen tensorflow model into memory
+  print("> ====== loading HAND frozen graph into memory")
+  detection_graph = tf.Graph()
+  with detection_graph.as_default():
+      od_graph_def = tf.GraphDef()
+      with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+          serialized_graph = fid.read()
+          od_graph_def.ParseFromString(serialized_graph)
+          tf.import_graph_def(od_graph_def, name='')
+      sess = tf.Session(graph=detection_graph)
+  print(">  ====== Hand Inference graph loaded.")
+  return detection_graph, sess
+
+def detect_objects(image_np, detection_graph, sess):
+  # Definite input and output Tensors for detection_graph
+  image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+  # Each box represents a part of the image where a particular object was detected.
+  detection_boxes = detection_graph.get_tensor_by_name(
+      'detection_boxes:0')
+  # Each score represent how level of confidence for each of the objects.
+  # Score is shown on the result image, together with the class label.
+  detection_scores = detection_graph.get_tensor_by_name(
+      'detection_scores:0')
+  detection_classes = detection_graph.get_tensor_by_name(
+      'detection_classes:0')
+  num_detections = detection_graph.get_tensor_by_name(
+      'num_detections:0')
+
+  image_np_expanded = np.expand_dims(image_np, axis=0)
+
+  (boxes, scores, classes, num) = sess.run(
+      [detection_boxes, detection_scores,
+          detection_classes, num_detections],
+      feed_dict={image_tensor: image_np_expanded})
+  return np.squeeze(boxes), np.squeeze(scores)
+
+
+if __name__ == "__main__":
+  import matplotlib.pyplot as plt
+  import matplotlib.patches as patches
+  from utils.captureVideo import cpatureVideo
+  import cv2
+
+  fd = cpatureVideo(WIDTH=480, HEIGHT=360)
+
+  fig, ax = plt.subplots()
+  detection_graph, sess = load_inference_graph()
+  print("Capture")
+
+  while True:
+    rgb_frame = fd.read()
+    gray_frame = fd.rgb2gray(rgb_frame)
+    boxes, scores = detect_objects(rgb_frame, detection_graph, sess)
+    print("boxes")
+    print(boxes)
+    print(scores)
+
+    plt.imshow(rgb_frame)
+    plt.pause(0.01)
+    plt.cla()
+
