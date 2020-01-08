3beb9d1bf6df07497b0dfe61b8612cc19fb5c2b6
k-washi <kai.washizaki@gmail.com>
2020-01-08 13:29:24 +0900

    Add faceDtector and mouth opening

--- detector/faceDetector.py
+++ detector/faceDetector.py
@@ -13,22 +13,27 @@ from imutils import face_utils
 
 #-----------
 from detector.tracking import trackingCV
-from utils.kalmanFilter import Postionlkf
+from utils.kalmanFilter import Positionlkf
 
 
 CV_MODEL_PATH = './model/haarcascade_frontalface_alt2.xml'
 assert os.path.isfile(CV_MODEL_PATH ), 'haarcascade_frontalface_default.xml がない'
 DLIB_MODEL_PATH = './model/shape_predictor_68_face_landmarks.dat'
+DLIB_CNN_FF_MODEL_PATH = './model/mmod_human_face_detector.dat'
 
 #DLIB id: numpyに合わせて0から始まるものとする
 DLIB_LEFT_EYE_ID = [42, 47]
 DLIB_RIGHT_EYE_ID = [36, 41]
+DLIB_CENTER_ID = 27
 DLIB_NOSE_ID = 33
 DLIB_CHIN_ID = 8
 DLIB_REYE_ID = 36
 DLIB_LEYE_ID = 45
 DLIB_RM_ID = 48
 DLIB_LM_ID = 54
+DLIB_MOUTH_LURB_ID = [60, 62, 64, 66]
+
+
 
 #HWIDTH, HHEIGHT = 1280, 720
 HWIDTH, HHEIGHT = 1920, 1080
@@ -44,9 +49,32 @@ FOCAL_LENGTH = HWIDTH
 CENTER = (HWIDTH/2, HHEIGHT/2)
 CAMERA_MATRIX = np.array([[FOCAL_LENGTH, 0, CENTER[0]],
                           [0, FOCAL_LENGTH, CENTER[1]],
-                          [0, 0, 1]], dtype=np.float64)
+                          [0, 0, 1]], dtype=np.float32)
  
 #The Male mean interpupillary distance is 64.7 mm (https://en.wikipedia.org/wiki/Interpupillary_distance)
+
+"""
+#X-Y-Z with X pointing forward and Y on the left.
+#The X-Y-Z coordinates used are like the standard
+# coordinates of ROS (robotic operative system)
+P3D_RIGHT_SIDE = np.float32([-100.0, -77.5, -5.0]) #0
+P3D_GONION_RIGHT = np.float32([-110.0, -77.5, -85.0]) #4
+P3D_MENTON = np.float32([0.0, 0.0, -122.7]) #8
+P3D_GONION_LEFT = np.float32([-110.0, 77.5, -85.0]) #12
+P3D_LEFT_SIDE = np.float32([-100.0, 77.5, -5.0]) #16
+P3D_FRONTAL_BREADTH_RIGHT = np.float32([-20.0, -56.1, 10.0]) #17
+P3D_FRONTAL_BREADTH_LEFT = np.float32([-20.0, 56.1, 10.0]) #26
+P3D_SELLION = np.float32([0.0, 0.0, 0.0]) #27
+P3D_NOSE = np.float32([21.1, 0.0, -48.0]) #30
+P3D_SUB_NOSE = np.float32([5.0, 0.0, -52.0]) #33
+P3D_RIGHT_EYE = np.float32([-20.0, -65.5,-5.0]) #36
+P3D_RIGHT_TEAR = np.float32([-10.0, -40.5,-5.0]) #39
+P3D_LEFT_TEAR = np.float32([-10.0, 40.5,-5.0]) #42
+P3D_LEFT_EYE = np.float32([-20.0, 65.5,-5.0]) #45
+#P3D_LIP_RIGHT = numpy.float32([-20.0, 65.5,-5.0]) #48
+#P3D_LIP_LEFT = numpy.float32([-20.0, 65.5,-5.0]) #54
+P3D_STOMION = np.float32([10.0, 0.0, -75.0]) #62
+"""
 MODEL_POINT = np.array([
                         (0.0, 0.0, 0.0),             # Nose tip
                         (0.0, -330.0, -65.0),        # Chin
@@ -57,21 +85,48 @@ MODEL_POINT = np.array([
                       
                     ], dtype=np.float64)
 
-DIST_COFFEIS = np.zeros((4,1)) #レンズ歪みなし
+"""
+TRACKED_POINTS = (0, 4, 8, 12, 16, 17, 26, 27, 30, 33, 36, 39, 42, 45, 62)
+MODEL_POINT  = np.float32([P3D_RIGHT_SIDE,
+                                  P3D_GONION_RIGHT,
+                                  P3D_MENTON,
+                                  P3D_GONION_LEFT,
+                                  P3D_LEFT_SIDE,
+                                  P3D_FRONTAL_BREADTH_RIGHT,
+                                  P3D_FRONTAL_BREADTH_LEFT,
+                                  P3D_SELLION,
+                                  P3D_NOSE,
+                                  P3D_SUB_NOSE,
+                                  P3D_RIGHT_EYE,
+                                  P3D_RIGHT_TEAR,
+                                  P3D_LEFT_TEAR,
+                                  P3D_LEFT_EYE,
+                                  P3D_STOMION])
+MODEL_POINT[:,0], MODEL_POINT[:,1], MODEL_POINT[:,2] = -MODEL_POINT[:,1], -MODEL_POINT[:,2], -MODEL_POINT[:,1]
+MODEL_POINT -= np.float32([P3D_NOSE])
+"""
+DIST_COFFEIS = np.zeros((4,1), dtype=np.float32) #レンズ歪みなし
+
+HEAD_POSE_NOISE = 2. #deg
+
+FACE_STD_DIR = np.array([[-63.94196692],[ 51.86835768],[ 69.18373256]]) * np.pi /180.
 
 
 class faceDetectorDlib():
   def __init__(self):
+    self.rotation_vector = np.array([[ 0.],[0.],[ 0.]])
     self.dlib_model_set()
 
   def dlib_model_set(self, file_path = DLIB_MODEL_PATH):
     self._detector = dlib.get_frontal_face_detector()
     #self._detector = cv2.CascadeClassifier(CV_MODEL_PATH)
     self._predictor = dlib.shape_predictor(file_path)
+    #self._predictor = dlib.cnn_face_detection_model_v1(DLIB_CNN_FF_MODEL_PATH)
 
   def detect(self, frame):
     count = 0
     landmark = None
+    
     low_frame = cv2.resize(frame, dsize=(LWIDTH, LHEIGHT))
     dets = self._detector(low_frame, 1) #rgb frame, upsample
     #dets = self._detector.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=5)
@@ -194,6 +249,7 @@ class faceDetectorDlib():
 
   def faceDirection(self, landmark):
     #https://www.learnopencv.com/head-pose-estimation-using-opencv-and-dlib/
+    
     image_points = np.array([landmark[DLIB_NOSE_ID],
                              landmark[DLIB_CHIN_ID],
                              landmark[DLIB_LEYE_ID],
@@ -201,7 +257,13 @@ class faceDetectorDlib():
                              landmark[DLIB_LM_ID],
                              landmark[DLIB_RM_ID]
                              ], dtype=np.float64)
+    """
+    track_point = []
+    for p in TRACKED_POINTS:
+      track_point.append(landmark[p]) 
+    image_points = np.array(track_point, dtype=np.float32)
     #print(image_points)
+    """
     """
     [[ 987.  527.]
     [ 978.  709.]
@@ -210,21 +272,25 @@ class faceDetectorDlib():
     [1046.  596.]
     [ 927.  600.]]
     """
-
-
-    
-    
-    (success, rotation_vector, translation_vector) = cv2.solvePnP(MODEL_POINT, image_points, CAMERA_MATRIX,DIST_COFFEIS, flags=cv2.SOLVEPNP_ITERATIVE)
+    (success, rotation_vector, translation_vector) = cv2.solvePnP(MODEL_POINT, image_points, CAMERA_MATRIX,DIST_COFFEIS)
     #print("SUCCESS: {0}".format(success))
-    print("ROTATION: {0}".format(rotation_vector * 180 / np.pi))
+    #rotation_vector -= FACE_STD_DIR
+    #print("ROTATION: {0}".format(rotation_vector * 180 / np.pi))
     
     #print("TRANS: {0}".format(translation_vector))
-    
-    (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, CAMERA_MATRIX, DIST_COFFEIS)
+    axis = np.array([(0.0, 0.0, 500.0)])
+    (nose_end_point2D, jacobian) = cv2.projectPoints(axis, rotation_vector, translation_vector, CAMERA_MATRIX, DIST_COFFEIS)
     return rotation_vector, translation_vector, nose_end_point2D
   
   def mouthOpenDetector(self, landmark):
-    pass
+    left = landmark[DLIB_MOUTH_LURB_ID[0]]
+    up = landmark[DLIB_MOUTH_LURB_ID[1]]
+    right = landmark[DLIB_MOUTH_LURB_ID[2]]
+    bottom = landmark[DLIB_MOUTH_LURB_ID[3]]
+
+    mw = np.linalg.norm(right - left)
+    mh = np.linalg.norm(up - bottom)
+    return mh/np.float(mw)
 
 
   def _show_landmark(self):
@@ -235,17 +301,26 @@ class faceDetectorDlib():
 
     fd = cpatureVideo(WIDTH=HWIDTH, HEIGHT=HHEIGHT, deviceID=0)
     tracker = trackingCV(kcf=True)
-    facePos = Postionlkf(HWIDTH/2., HHEIGHT/2.)
+    facePos = Positionlkf(HWIDTH/2., HHEIGHT/2.)
     falseCounter = 0
     falseTh = 20
     pos = np.array([int(HWIDTH/2), int(HHEIGHT/2)]) 
-    old_pos = np.array([int(HWIDTH/2), int(HHEIGHT/2)])
+    old_pos = np.array([int(HWIDTH/2), int(HHEIGHT/2)]) 
+    rect_pos = np.array([int(HWIDTH/2), int(HHEIGHT/2)])
+    rect_vel = np.array([0, 0])
+
+    dlib_noise_pix = 30
+    true_pos = np.array([int(HWIDTH/2), int(HHEIGHT/2)])
+
+    true_rotationV = np.array([[0.], [0.], [0.]])
+    rotV = np.array([[0.], [0.], [0.]]) 
+    true_noseP = None
 
     #fig, ax = plt.subplots()
 
     while True:
       rgb_frame = fd.read()
-      rgb_frame = rgb_frame.copy()
+      #rgb_frame = rgb_frame.copy()
       #rgb_frame = fd.resize(rgb_frame, LWIDTH, LHEIGHT)
       
       if rgb_frame is None:
@@ -265,10 +340,14 @@ class faceDetectorDlib():
         
       if det is not None and landmark is not None:
         rotV, tranV, noseP = self.faceDirection(landmark)
-        cv2.line(rgb_frame, (int(landmark[DLIB_NOSE_ID][0]), int(landmark[DLIB_NOSE_ID][1])),
-                  (int(noseP[0][0][0]), int(noseP[0][0][1])), (255, 0, 0), 2)
+        
         #self.eyeBlinkDetect(landmark)
         #eyem = self.eyeTracker(rgb_frame,landmark)
+        
+
+        
+
+
         #rect = patches.Rectangle((det.left(), det.top()), det.width(), det.height(), fill=False)
         centerw = det.left() + det.width()/2
         centert = det.top() + det.height()/2
@@ -276,7 +355,19 @@ class faceDetectorDlib():
      
         #self.tracker_init(gray_frame, det.left(), det.top(), det.width(), det.height())
         tracker.tracker_init(hsv_frame, centerw - width, centert - width, width * 2, width * 2)
+        track, rect = tracker.tracking(hsv_frame)
+        if track:
+          rect_pos[0],rect_pos[1] = int(rect[0] + rect[2]/2), int(rect[1] + rect[3]/2)
+          
+          #facePos.calcVelocity( ) 
+          #pos, sigma = facePos.estimate()
+        nosePos = landmark[DLIB_CENTER_ID]
+        #pos, _ = facePos.update(nosePos[0], nosePos[1])
+        old_pos = nosePos.copy()
+        #print('landmark pos {0}  : {1}'.format(pos, nosePos))
 
+        m_open_ratio = self.mouthOpenDetector(landmark)
+        print("mouth open {0}".format(m_open_ratio))
         #ax.add_patch(rect)
         '''
         left_eye = landmark[DLIB_LEFT_EYE_ID[0]: DLIB_LEFT_EYE_ID[1]+1]
@@ -290,34 +381,59 @@ class faceDetectorDlib():
           cv2.circle(rgb_frame, (int(x), int(y)), 3, (0,0,255), -1)
           #plt.scatter(x, y)
           #plt.text(x, y, k)
+      else:  
+      
+        track, rect = tracker.tracking(hsv_frame)
         
-      
-      track, rect = tracker.tracking(hsv_frame)
-      
-      
-      if track:
-        cv2.rectangle(rgb_frame, (int(rect[0]), int(rect[1])), (int(rect[0] + rect[2]), int(rect[1] + rect[3])), (0, 255, 0), 2)
-        #rect = patches.Rectangle((rect[0], rect[1]), rect[2], rect[3], fill=False)
-        #ax.add_patch(rect)
-        
-        falseCounter = 0
-        pos, sigma = facePos.update(int(rect[0] + rect[2]/2), int(rect[1] + rect[3]/2) )
-        old_pos = pos
+        if track:
+          cv2.rectangle(rgb_frame, (int(rect[0]), int(rect[1])), (int(rect[0] + rect[2]), int(rect[1] + rect[3])), (0, 255, 0), 2)
+          #rect = patches.Rectangle((rect[0], rect[1]), rect[2], rect[3], fill=False)
+          #ax.add_patch(rect)
+          print("tracking")
+          falseCounter = 0
 
-        #print("pos {0}, Sigma {1}".format(pos, sigma))
-        
-        
-      else:
-        falseCounter += 1
-        print("opos {0}".format(old_pos))
-        if falseCounter < falseTh:
-          pos, sigma = facePos.update(old_pos[0], old_pos[1])
+          rect_vel[0], rect_vel[1] = int(rect[0] + rect[2]/2) - rect_pos[0], int(rect[1] + rect[3]/2) - rect_pos[1] 
+          rect_pos[0], rect_pos[1] = int(rect[0] + rect[2]/2), int(rect[1] + rect[3]/2) 
+
+          old_pos[0], old_pos[1] = old_pos[0] + rect_vel[0], old_pos[1] + rect_vel[1]
+          #pos, sigma = facePos.estimate()
+          
+          #print('track pos {0}, {1} '.format(pos, int(rect[0] + rect[2]/2)))
+
+          
+          
         else:
-          pos, sigma = facePos.update(int(HWIDTH/2), int(HHEIGHT/2) )
-        #print("fail track")
-      print("pos {0}".format(pos))
-      cv2.circle(rgb_frame, (int(pos[0]), int(pos[1])), 10, (0,255,0), -1)
+          falseCounter += 1
+          #print("opos {0}".format(old_pos))
+          print("tracking fail")
+          if falseCounter < falseTh:
+            #uw, uh = facePos.calcVelocity(old_pos[0], old_pos[1])
+            #print(uw, uh) 
+            rect_vel = rect_vel * 0.9
+            old_pos[0], old_pos[1] = old_pos[0] + rect_vel[0], old_pos[1] + rect_vel[1]
+            
+            #pos, sigma = facePos.estimate()
+            #print('track F pos {0}  : {1},, {2}'.format(pos, uw, old_pos))
+          else:
+            #facePos.calcVelocity(int(HWIDTH/2), int(HHEIGHT/2) )
+            #pos, sigma = facePos.update(int(HWIDTH/2), int(HHEIGHT/2) )
+            #print('track FF pos {0}  : {1}'.format(pos, uw))
+            old_pos[0], old_pos[1] = old_pos[0] + (int(HWIDTH/2) - old_pos[0])*0.3, old_pos[1] + (int(HHEIGHT/2) - old_pos[1])*0.3
+          
+      if np.abs(true_pos[0] - old_pos[0]) > dlib_noise_pix or np.abs(true_pos[1] - old_pos[1]) > dlib_noise_pix:
+        true_pos = old_pos
+        true_rotationV = rotV
+        true_noseP = noseP
+        print(true_rotationV)
+      
+      if true_noseP is not None and landmark is not None:
+        cv2.line(rgb_frame, (int(landmark[DLIB_NOSE_ID][0]), int(landmark[DLIB_NOSE_ID][1])),
+                  (int(true_noseP[0][0][0]), int(true_noseP[0][0][1])), (255, 0, 0), 2)
+
+      #print("oldpos {0}".format(old_pos))
+      cv2.circle(rgb_frame, (int(true_pos[0]), int(true_pos[1])), 10, (0,255,0), -1)
       cv2.imshow('img', rgb_frame)
+
       if cv2.waitKey(1) & 0xFF == ord('q'):
         break
       #plt.imshow(rgb_frame)
--- utils/kalmanFilter.py
+++ utils/kalmanFilter.py
@@ -1,52 +1,13 @@
 import numpy as np
 
-class Postionlkf():
-  def __init__(self, init_w, init_h):
-    self.T = 1.
-    #state space model x = A*x + B*u + w, w ~ N(0, Q)
-    self.A = np.array([[1, 0, self.T, 0], [0, 1, 0, self.T], [0, 0, 1, 0], [0, 0, 0, 1]])
-    #self.B = np.array([[1,0], [0, 1]])
-    self.Q = np.array([[100, 0, 0, 0],[0, 100, 0, 0], [0, 0, 500, 0], [0, 0, 0, 500]]) #移動誤差
-
-    #observe model y = C * x + v, v ~ N(0, R)
-    self.C = np.array([[1,0, 0, 0], [0,1, 0, 0]])
-    self.R = np.array([[200, 0],[0, 200]]) #計測誤差
-
-    self.x = np.array([[init_w], [init_h], [0], [0]])
-
-    self.mu = np.array([[0], [0], [0], [0]])
-    self.Sigma = np.array([[0, 0, 0, 0],[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
-
-
-  def update(self, w, h, update = True):
-    #estiarrayion
-    
-    mu_ = np.dot(self.A, self.x) #+u速度の内界情報なし #現在の推定値
-    #print("mu_ {0}".format(mu_))
-    Sigma_ = self.Q + np.dot(np.dot(self.A, self.Sigma), self.A.T) #現在の誤差行列
-
-    #update
-    if update:
-      yi = np.array([[w], [h]]) - np.dot(self.C, mu_) #観測残差
-      #print("yi {0}".format(yi))
-      S = np.dot(np.dot(self.C, Sigma_), self.C.T) + self.R #観測残差の共分散
-      K = np.dot(np.dot(Sigma_, self.C.T), np.linalg.inv(S))  #カルマンゲイン
-      self.x = mu_ + np.dot(K, yi)#更新された現在の推定値
-      self.Sigma = Sigma_ - np.dot(np.dot(K, self.C), Sigma_)#更新された現在の誤差行列
-      print("x {0}".format(self.x))
-    
-      return np.array([self.x[0][0], self.x[1][0]]), self.Sigma
-    
-    return np.array([mu_[0][0], mu_[0][1]]), Sigma_
-
-class Rotationlkf():
-  def __init__(self, init_x, init_y, init_z):
+class Positionlkf():
+  def __init__(self, init_x, init_y):
     
     self.T = 1.
     #state space model x = A*x + B*u + w, w ~ N(0, Q)
-    self.A = np.array([[1, 0, self.T, 0], [0, 1, 0, self.T], [0, 0, 1, 0], [0, 0, 0, 1]])
-    #self.B = np.array([[1,0], [0, 1]])
-    self.Q = np.array([[100, 0, 0, 0],[0, 100, 0, 0], [0, 0, 500, 0], [0, 0, 0, 500]]) #移動誤差
+    self.A = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    self.B = np.array([[0,0, self.T, 0], [0, 0, 0, self.T], [0, 0, 0, 0], [0, 0, 0, 0]])
+    self.Q = np.array([[50, 0, 0, 0],[0, 50, 0, 0], [0, 0, 100, 0], [0, 0, 0, 100]]) #移動誤差
 
     #observe model y = C * x + v, v ~ N(0, R)
     self.C = np.array([[1,0, 0, 0], [0,1, 0, 0]])
@@ -56,25 +17,40 @@ class Rotationlkf():
 
     self.mu = np.array([[0], [0], [0], [0]])
     self.Sigma = np.array([[0, 0, 0, 0],[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+    self.mu_ = np.array([[0], [0], [0], [0]])
+    self.Sigma_ = np.array([[0, 0, 0, 0],[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]) 
 
+    self.e_w = init_x
+    self.e_h = init_y
+    self.uw = 0
+    self.uh = 0
 
-  def update(self, w, h, update = True):
+  def estimate(self):
     #estiarrayion
     
-    mu_ = np.dot(self.A, self.x) #+u速度の内界情報なし #現在の推定値
-    #print("mu_ {0}".format(mu_))
-    Sigma_ = self.Q + np.dot(np.dot(self.A, self.Sigma), self.A.T) #現在の誤差行列
+    self.mu_ = np.dot(self.A, self.x) + np.dot(self.B, np.array([[0], [0], [self.uw], [self.uh]])) #u速度の内界情報としてtrackerの移動量を与える #現在の推定値
+    print("mu_ {0}, {1}, {2}".format(self.mu_, self.uw, self.x))
+    self.Sigma_ = self.Q + np.dot(np.dot(self.A, self.Sigma), self.A.T) #現在の誤差行列
+    return np.array([self.mu_[0][0], self.mu_[1][0]]), self.Sigma_
 
+  def update(self, w, h):
     #update
-    if update:
-      yi = np.array([[w], [h]]) - np.dot(self.C, mu_) #観測残差
-      #print("yi {0}".format(yi))
-      S = np.dot(np.dot(self.C, Sigma_), self.C.T) + self.R #観測残差の共分散
-      K = np.dot(np.dot(Sigma_, self.C.T), np.linalg.inv(S))  #カルマンゲイン
-      self.x = mu_ + np.dot(K, yi)#更新された現在の推定値
-      self.Sigma = Sigma_ - np.dot(np.dot(K, self.C), Sigma_)#更新された現在の誤差行列
-      print("x {0}".format(self.x))
-    
-      return np.array([self.x[0][0], self.x[1][0]]), self.Sigma
-    
-    return np.array([mu_[0][0], mu_[0][1]]), Sigma_
+    #鼻先の位置を与える
+    yi = np.array([[w], [h]]) - np.dot(self.C, self.mu_) #観測残差
+    #print("yi {0}".format(yi))
+    S = np.dot(np.dot(self.C, self.Sigma_), self.C.T) + self.R #観測残差の共分散
+    K = np.dot(np.dot(self.Sigma_, self.C.T), np.linalg.inv(S))  #カルマンゲイン
+    self.x = self.mu_ + np.dot(K, yi)#更新された現在の推定値
+    self.Sigma = self.Sigma_ - np.dot(np.dot(K, self.C), self.Sigma_)#更新された現在の誤差行列
+    #print("x {0}".format(self.x))
+
+    return np.array([self.x[0][0], self.x[1][0]]), self.Sigma
+  
+  def calcVelocity(self, w, h):
+    self.uw = w - self.e_w
+    self.uh = h - self.e_h
+    self.e_w = w
+    self.e_h = h
+
+    #return uw, uh
+  
